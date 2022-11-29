2c88d9cca03226eebc945d5f0c012ac2bc1d312f
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-11-29 17:35:58 +0200

    HW 10 draft

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
-const filterOptions = '?fields=name,capital,population,flags,languages';
-
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const filterOptions = '?fields=name,capital,population,flags,languages';
 
 export default function fetchCountries(inputValue) {
   return fetch(
@@ -9,6 +9,7 @@ export default function fetchCountries(inputValue) {
   ).then(r => {
     if (!r.ok) {
       Notiflix.Notify.failure('Oops, there is no country with that name');
+      return;
     }
 
     return r.json();
--- src/index.js
+++ src/index.js
@@ -12,62 +12,80 @@ const countryListWrap = document.querySelector('.country-list');
 const countryInfoWrap = document.querySelector('.country-info');
 const input = document.querySelector('#search-box');
 
-function renderList(countriesList) {
-  const markup = countriesList.map(country => {
-    console.log(country);
-
-    return `<li>
-          <img src="${country.flags.svg}" alt="flag" width="40" />
-          <p>${country.name}</p>
-        </li>`;
-  });
-
-  countryListWrap.insertAdjacentHTML('beforeend', markup.join(''));
-}
-
-function renderInfo(countryInfo) {
-  console.log(countryInfo);
-  const contryLanguages = countryInfo.languages.map(language => language.name);
-  const markup = `
-          <div class = "country-name"><img src="${
-            countryInfo.flags.svg
-          }" alt="flag" width="40" />
-          <p>${countryInfo.name}</p></div>
-          <p>Capital: <span>${countryInfo.capital}</span></p>
-          <p>Population: <span>${countryInfo.population}</span></p>
-          <p>Languages: <span>${contryLanguages.join(', ')}</span></p>
-        `;
-
-  countryInfoWrap.insertAdjacentHTML('beforeend', markup);
-}
-
 function clearMarkup() {
   countryListWrap.innerHTML = '';
   countryInfoWrap.innerHTML = '';
 }
 
-function onInputChange() {
-  const inputValue = input.value;
+function renderList(countriesList) {
   clearMarkup();
-  if (inputValue) {
-    fetchCountries(inputValue)
-      .then(countries => {
-        if (countries.length > 10) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
-          return;
-        }
-        if (countries.length === 1) {
-          renderInfo(countries[0]);
-          return;
-        }
-        if (countries.length > 1 && countries.length <= 10) {
-          renderList(countries);
-        }
-      })
-      .catch(console.log);
+
+  const markup = countriesList.map(country => {
+    return `
+        <li>
+          <img src="${country.flags.svg}" alt="flag" width="40" />
+          <p>${country.name}</p>
+        </li>
+        <hr>`;
+  });
+
+  // const markup = countriesList.reduce(
+  //   (acc, country) => {
+
+  //     acc += `
+  //       <li>
+  //         <img src="${country.flags.svg}" alt="flag" width="40" />
+  //         <p>${country.name}</p>
+  //       </li>
+  //       <hr>`;
+  //     return acc;
+  //   },
+
+  //   ''
+  // );
+
+  countryListWrap.insertAdjacentHTML('beforeend', markup.join(''));
+}
+
+function renderInfo(countryInfo) {
+  clearMarkup();
+
+  const contryLanguages = countryInfo.languages.map(language => language.name);
+  const markup = `
+          <div class = "country-name">
+            <img src="${countryInfo.flags.svg}" alt="flag" width="40" />
+            <p>${countryInfo.name}</p>
+          </div>
+          <p><span>Capital: </span>${countryInfo.capital}</p>
+          <p><span>Population: </span>${countryInfo.population}</p>
+          <p><span>Languages: </span>${contryLanguages.join(', ')}</p>
+        `;
+
+  countryInfoWrap.insertAdjacentHTML('beforeend', markup);
+}
+
+function onInputChange(e) {
+  const inputValue = e.target.value.trim();
+
+  if (!inputValue) {
+    clearMarkup();
+    return;
   }
+  fetchCountries(inputValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (countries.length === 1) {
+        renderInfo(countries[0]);
+        return;
+      }
+      renderList(countries);
+    })
+    .catch(console.log);
 }
 
 input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
